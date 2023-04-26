992115c6cbcda3cc129b9da449409032d8a97ea0
Noiana Lagras <noi-lagras@hotmail.com>
2023-04-26 00:52:10 -0300

    corrigiendo

--- src/components/Productos/Card.jsx
+++ src/components/Productos/Card.jsx
@@ -1,13 +1,22 @@
-import React from 'react'
+import React, { useState } from 'react'
 function Card (props) {
-  
+  const [numero,setNumero] = useState(0)
+  const sumar = () => {
+    setNumero(numero+1)
+  }
+  const restar = () => {
+    if (numero>0){
+      setNumero(numero-1)
+    }
+  }
   return (
     <div className='card'>
         <img src={props.img} alt={props.name} />
+        <p className='cantidadCard'>{numero}</p>
         <h3>{props.name}</h3>
         <p>${props.price}</p>
-        <button className= "addCart">Agregar al Carrito</button>
-        <button className='addCart'>Quitar del Carrito</button>
+        <button className= "addCart" onClick={sumar}>Agregar al Carrito</button>
+        <button className='addCart' onClick={restar}>Quitar del Carrito</button>
     </div>
   )
 }
